bf9009a35a183f9b762bff3c94e9ad6d95621dbb
Jesse <jayesbe@users.noreply.github.com>
2017-07-17 16:55:02 -0400

    logarithmic sliding scale

--- converters.js
+++ converters.js
@@ -36,10 +36,23 @@ export function createArray(start, end, step) {
     //console.log('invalid step: ', step);
     return result;
   } else {
-    length = Math.abs((start - end) / step) + 1;
-    for (i = 0; i < length; i++) {
-      result.push(start + i * Math.abs(step) * direction);
+    // position will be between 0 and step
+    var minp = 0;
+    var maxp = step;
+
+    // The result should be between start an end
+    var minv = Math.log(start <= 0 ? 1 : start);
+    var maxv = Math.log(end);
+
+    // calculate adjustment factor
+    var scale = (maxv - minv) / (maxp - minp);
+    
+    for (i = 0; i < step; ++i) {
+      result.push(Math.round(Math.exp(minv + scale * (i - minp))));
     }
+    result.unshift(start);
+    result.push(end);
+    
     return result;
   }
 }
